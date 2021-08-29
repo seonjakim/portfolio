ee43963bb3600a292f463ca18cb4baa11b672e8a
seonja <seonja.kim0@gmail.com>
2021-08-29 12:59:29 +0900

    inital setting

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,31 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+
 module.exports = {
-  entry: "./src/index.js",
+  entry: './index.js',
   output: {
-    path: path.resolve(__dirname, "public"),
-    publicPath: '/public/',
-    filename: 'main.js'
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.js',
   },
   module: {
     rules: [
       {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+      },
+      {
         test: /\.css$/,
-        use: ['style-loader', 'css-loader']
-      }
-    ]
-  },
-  devServer: {
-    contentBase: path.join(__dirname, 'public'),
-    port: 9000
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
   },
   plugins: [
     new HtmlWebpackPlugin({
       template: './index.html',
-      filename: './index.html'
-    })
-  ]
+    }),
+    new CleanWebpackPlugin(),
+  ],
+  devtool: 'source-map',
 }
-
-//npx webpack --watch
